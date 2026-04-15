2f58654283d2d1a779413f7f4d091539c56b8f32
nqhoang011 <meow.h.sunrise@gmail.com>
2026-04-16 00:21:20 +0700

    feat: add authentication apis

--- chatapp/src/main/java/com/nqhoang011/chatapp/controller/AuthenticationController.java
+++ chatapp/src/main/java/com/nqhoang011/chatapp/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.nqhoang011.chatapp.controller;
 
+import com.nimbusds.jose.JOSEException;
 import com.nqhoang011.chatapp.dto.request.ApiResponse;
 import com.nqhoang011.chatapp.dto.request.AuthenticationRequest;
+import com.nqhoang011.chatapp.dto.request.IntrospectRequest;
 import com.nqhoang011.chatapp.dto.response.AuthenticationResponse;
+import com.nqhoang011.chatapp.dto.response.IntrospectResponse;
 import com.nqhoang011.chatapp.service.AuthenticationService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,13 +26,20 @@ public class AuthenticationController {
 
     @PostMapping("/log-in")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        Boolean result = authenticationService.authenticate(request);
+        AuthenticationResponse result = authenticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder()
                 .code(200)
-                .result(AuthenticationResponse
-                        .builder()
-                        .isAuthenticated(result)
-                        .build())
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        IntrospectResponse result = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder()
+                .code(200)
+                .result(result)
                 .build();
     }
 }
