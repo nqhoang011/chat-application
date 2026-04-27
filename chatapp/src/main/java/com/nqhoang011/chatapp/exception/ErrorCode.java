6ebbb0ff1c33cece1285afd15907083742ab1dc4
nqhoang011 <meow.h.sunrise@gmail.com>
2026-04-27 18:57:24 +0700

    feat: Handling exception and return with custom Http status

--- chatapp/src/main/java/com/nqhoang011/chatapp/exception/ErrorCode.java
+++ chatapp/src/main/java/com/nqhoang011/chatapp/exception/ErrorCode.java
@@ -2,18 +2,22 @@ package com.nqhoang011.chatapp.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_EXISTED(1001, "User existed"),
-    UNHANDLED_EXCEPTION(9999, "Unhandled exception"),
-    INVALID_KEY(9998, "Invalid message key"),
-    INVALID_USERNAME(1002, "Username must be at least 3 characters"),
-    INVALID_PASSWORD(1003, "Password must be at least 8 characters"),
-    USER_NOT_EXISTED(1004, "User not existed"),
-    UNAUTHENTICATED(1005, "Unauthenticated")
+    USER_EXISTED(1001, "User existed", HttpStatus.BAD_REQUEST),
+    UNHANDLED_EXCEPTION(9999, "Unhandled exception", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(9998, "Invalid message key", HttpStatus.BAD_REQUEST),
+    INVALID_USERNAME(1002, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1003, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1004, "User not existed", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1005, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1006, "You do not have permission", HttpStatus.FORBIDDEN)
     ;
     private int code;
     private String message;
+    private HttpStatusCode statusCode;
 }
--- chatapp/src/main/java/com/nqhoang011/chatapp/exception/GlobalExceptionHandler.java
+++ chatapp/src/main/java/com/nqhoang011/chatapp/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.nqhoang011.chatapp.exception;
 import com.nqhoang011.chatapp.dto.request.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,7 +30,20 @@ public class GlobalExceptionHandler {
         response.setCode(errorCode.getCode());
         response.setMessage(errorCode.getMessage());
 
-        return ResponseEntity.badRequest().body(response);
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(response);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handleAccessDeniedException(AccessDeniedException exception) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
